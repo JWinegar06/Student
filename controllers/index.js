72d3c493d8e8786e396c48e2296678d2287d425f
JWinegar06 <winegaj86@gmail.com>
2026-02-10 14:20:01 -0700

    2/10 Updates

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,24 @@
-const awesomeFunction = (Req, res) => {
-  res.send("Hello World!");
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
+
+const awesomeFunction = (req, res) => {
+  res.send("Awesome Name!");
 };
 
-const tooeleTech = (Req, res) => {
-  res.send("Tooele Tech is Awesome!");
+const tooeleTechFunction = (req, res) => {
+  res.send("Eikon Tech is Awesome!");
 };
 
-module.exports = { awesomeFunction, tooeleTech };
+const getAllStudents = async (req, res) => {
+  try {
+    const result = await mongodb.getDb().db().collection("students").find();
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch(error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { awesomeFunction, tooeleTechFunction, getAllStudents };
